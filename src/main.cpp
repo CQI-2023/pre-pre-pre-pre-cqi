b2c79f1a38b24cca7a0d313cf3d7a4f9e4baba99
MrMushroomkiller <mr.mushroomkiller@gmail.com>
2022-11-19 16:05:09 -0500

    init

---
+++ src/main.cpp
@@ -0,0 +1,204 @@
+#include <Arduino.h>
+
+#define ENA 14  // Enable/speed motors Right
+#define ENB 12  // Enable/speed motors Left
+#define IN_1 15 // L298N in1 motors Rightx
+#define IN_2 13 // L298N in2 motors Right
+#define IN_3 3  // L298N in3 motors Left
+#define IN_4 5  // L298N in4 motors Left
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+String command;     // String to store app command state.
+String lastCommand; // String to store app command state.
+int speedCar = 800; // 400 - 1023.
+int speed_Coeff = 3;
+bool cringe = false;
+
+const char *ssid = "Make DIY";
+ESP8266WebServer server(80);
+
+void HTTP_handleRoot(void)
+{
+  if (server.hasArg("State"))
+  {
+    Serial.println(server.arg("State"));
+  }
+  server.send(200, "text/html", "");
+  delay(1);
+}
+
+void setup()
+{
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  // Connecting WiFi
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  // Starting WEB-server
+  server.on("/", HTTP_handleRoot);
+  server.onNotFound(HTTP_handleRoot);
+  server.begin();
+}
+
+void goAhead()
+{
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+  analogWrite(ENB, speedCar);
+}
+
+void goBack()
+{
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar);
+}
+
+void goRight()
+{
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+  analogWrite(ENB, speedCar);
+}
+
+void goLeft()
+{
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar);
+}
+
+void goAheadRight()
+{
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+  analogWrite(ENA, speedCar / speed_Coeff);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+  analogWrite(ENB, speedCar);
+}
+
+void goAheadLeft()
+{
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+  analogWrite(ENB, speedCar / speed_Coeff);
+}
+
+void goBackRight()
+{
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar / speed_Coeff);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar);
+}
+
+void goBackLeft()
+{
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar / speed_Coeff);
+}
+
+void stopRobot()
+{
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar);
+}
+
+void loop()
+{
+  server.handleClient();
+  command = server.arg("State");
+  if (command != lastCommand)
+  {
+    // Serial.println(command);
+    if (command == "S")
+      stopRobot();
+    // else if (command == "B") goBack();
+    // else if (command == "L") goLeft();
+    // else if (command == "R") goRight();
+    // else if (command == "I") goAheadRight();
+    // else if (command == "G") goAheadLeft();
+    // else if (command == "J") goBackRight();
+    // else if (command == "H") goBackLeft();
+    // else if (command == "0") speedCar = 400;
+    // else if (command == "1") speedCar = 470;
+    // else if (command == "2") speedCar = 540;
+    // else if (command == "3") speedCar = 610;
+    // else if (command == "4") speedCar = 680;
+    // else if (command == "5") speedCar = 750;
+    // else if (command == "6") speedCar = 820;
+    // else if (command == "7") speedCar = 890;
+    // else if (command == "8") speedCar = 960;
+    // else if (command == "9") speedCar = 1023;
+    // else if (command == "S") stopRobot();
+    else if (command == "L")
+    {
+      cringe = !cringe;
+      digitalWrite(LED_BUILTIN, cringe);
+      Serial.println("WTH BRO");
+    }
+    lastCommand = command;
+  }
+}
